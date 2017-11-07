3fa5450985deced2090670cb6272e5dc4eaff9bb
Amandir01 <derina_idryl@hotmail.com>
2017-11-07 16:51:15 -0500

    saving example

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var board = document.querySelector('.board-js') ;
+  board.addEventListener('click', addX);
+};
+var centinel = true ;
+function addX(event){
+  if (event.target.matches('td')&& event.target.textContent==='') {
+  if(centinel)
+    event.target.textContent = 'x';
+   else
+    event.target.textContent = '0';
+    centinel =!centinel ;
+  }
+}
